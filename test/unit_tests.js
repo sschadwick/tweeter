3481104cce4f3d04ddd2f4207bcaf3dc5ec3f1af
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-05-19 13:51:09 -0700

    added retweet and unretweet

--- test/unit_tests.js
+++ test/unit_tests.js
@@ -3,11 +3,16 @@ var expect = chai.expect;
 
 var addFollow = require(__dirname + '/../lib/addFollow');
 var unFollow = require(__dirname + '/../lib/unFollow');
+var retweet = require(__dirname + '/../lib/retweet');
+var unretweet = require(__dirname + '/../lib/unretweet');
+
+var randomName = 'mattblaze';
+var randomId = '730617219730669569';
 
 describe('Twitter API', function() {
 
   it('should be able to follow a user', function(done) {
-    addFollow('mattblaze', function(err, res) {
+    addFollow(randomName, function(err, res) {
       expect(err).to.eql(null);
       expect(res.screen_name).to.eql('mattblaze');
       expect(res.following).to.eql(true);
@@ -16,10 +21,28 @@ describe('Twitter API', function() {
   });
 
   it('should be able to unfollow a user', function(done) {
-    unFollow('mattblaze', function(err, res) {
+    unFollow(randomName, function(err, res) {
       expect(err).to.eql(null);
       expect(res.screen_name).to.eql('mattblaze');
       done();
     });
   });
+
+  it('should be able to retweet', function(done) {
+    retweet(randomId, function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.retweeted).to.eql(true);
+      expect(typeof res.retweeted_status).to.eql('object');
+      done();
+    });
+  });
+
+  it('should be able to unretweet', function(done) {
+    unretweet(randomId, function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.id_str).to.eql(randomId);
+      done();
+    });
+  });
+
 });

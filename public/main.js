b2113d8772d4537a344642680a08e647e1c1a2c7
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-05-20 17:47:54 -0700

    fixed loading issue

--- public/main.js
+++ public/main.js
@@ -12,26 +12,29 @@ $(function() {
   function loadCookies() {
     if (Cookies.get('keys')) {
       var keys = JSON.parse(Cookies.get('keys'));
+      console.log($('#consumer_key'))
       $('#consumer_key').val(keys.consumer_key);
       $('#consumer_secret').val(keys.consumer_secret);
       $('#access_token').val(keys.access_token);
       $('#access_token_secret').val(keys.access_token_secret);
+      // this is only updating the placeholder, not the value of the input
     }
   }
 
-  var formData = JSON.parse(JSON.stringify($("#authForm").serializeArray()));
-  var authObj = {};
-  for (var i in formData) {
-    if (formData[i].name) {
-      authObj[formData[i].name] = formData[i].value;
-    }
-  }
-  var str = JSON.stringify(authObj);
   loadCookies();
 
   $('#submit').on('click', function(e) {
+    var formData = JSON.parse(JSON.stringify($("#authForm").serializeArray()));
+    var authObj = {};
+    for (var i in formData) {
+      if (formData[i].name) {
+        authObj[formData[i].name] = formData[i].value;
+      }
+    }
+    var str = JSON.stringify(authObj);
     e.preventDefault();
     sendData(str);
+    $('#submitSuccess').html('Keys sent successfully!');
   });
 
   $('#saveKeys').on('click', function(e) {
@@ -42,6 +45,7 @@ $(function() {
       access_token: $('#access_token').val(),
       access_token_secret: $('#access_token_secret').val()
     }, { expires: 365 });
+    $('#saveSuccess').html('Keys saved!');
   });
 
 });

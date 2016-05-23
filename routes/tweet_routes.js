388666f6cf05347d8e335a671fbdaaa924170d7f
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-05-23 10:31:02 -0700

    commented routes

--- routes/tweet_routes.js
+++ routes/tweet_routes.js
@@ -5,36 +5,42 @@ var jsonParser = require('body-parser').json();
 
 var tweetRoute = module.exports = exports = express.Router();
 
-// Searching
+// SEARCHING: (returns 10 most recent tweets matching str)
 
 tweetRoute.get('/search/:str', function(req, res) {
   require(__dirname + '/../lib/search')(req, res);
 });
 
-// Following
+// FOLLOWING:
 
+// Add Follow
 tweetRoute.post('/follow/:id', jsonParser, function(req, res) {
   require(__dirname + '/../lib/addFollow2')(req, res);
 });
 
+// Un-Follow
 tweetRoute.post('/unfollow/:id', jsonParser, function(req, res) {
   require(__dirname + '/../lib/unFollow2')(req, res);
 });
 
-// Tweeting
+// TWEETING:
 
+// New tweet
 tweetRoute.post('/tweet', jsonParser, function(req, res) {
   require(__dirname + '/../lib/tweet2')(req, res);
 });
 
+// Un-tweet
 tweetRoute.post('/untweet/:tweetId', jsonParser, function(req, res) {
   require(__dirname + '/../lib/untweet2')(req, res);
 });
 
+// Re-tweet
 tweetRoute.post('/retweet/:tweetId', jsonParser, function(req, res) {
   require(__dirname + '/../lib/retweet2')(req, res);
 });
 
+// Unre-tweet
 tweetRoute.post('/unretweet/:tweetId', jsonParser, function(req, res) {
   require(__dirname + '/../lib/unretweet2')(req, res);
 });

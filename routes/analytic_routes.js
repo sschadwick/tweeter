bec0214e0cecff53749e2b533a1de73d83f1607f
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-06-03 16:12:26 -0700

    added analytics route

--- routes/analytic_routes.js
+++ routes/analytic_routes.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var jsonParser = require('body-parser').json();
+var fs = require('fs');
 
 var eatAuth = require(__dirname + '/../lib/eat_authentication');
 var responseHandler = require(__dirname + '/../lib/response_handler');
@@ -12,5 +13,9 @@ var logger = require(__dirname + '/../lib/logger');
 
 // TODO: change this to return the spreadsheet info
 analyticRouter.get('/analytics', function(req, res) {
-  logger(req, res);
+  fs.readFile(__dirname + '/../file.xls', function(err, data) {
+    if (err) {res.write('File not found');} else {
+      res.json(data.toString());
+    }
+  });
 });

a2d9ae4b8f3c660a6a27144e5ad646d91da510a2
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-06-06 08:46:50 -0700

    add basic ui colors, user-specific analytics, removed unused mysql install

--- routes/analytic_routes.js
+++ routes/analytic_routes.js
@@ -4,16 +4,15 @@ var express = require('express');
 var jsonParser = require('body-parser').json();
 var fs = require('fs');
 
+var logger = require(__dirname + '/../lib/logger');
 var eatAuth = require(__dirname + '/../lib/eat_authentication');
 var responseHandler = require(__dirname + '/../lib/response_handler');
 
 var analyticRouter = module.exports = exports = express.Router();
 
-var logger = require(__dirname + '/../lib/logger');
-
-analyticRouter.get('/analytics', function(req, res) {
-  fs.readFile(__dirname + '/../file.xls', function(err, data) {
-    if (err) {res.write('File not found');} else {
+analyticRouter.get('/analytics/:username', function(req, res) {
+  fs.readFile(__dirname + '/../data/' + req.params.username + '.xls', function(err, data) {
+    if (err) {res.json('File not found');} else {
       res.json(data.toString());
     }
   });
